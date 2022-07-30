9e4af03d6aabab954c2398384bfe81e2a1988d78
SHAHZAIB SHAFIQ <73356944+shahzaibshafiq100@users.noreply.github.com>
2022-07-30 22:32:17 +0500

    F

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React from 'react'
-import {BrowserRouter as Router ,Routes,Route,Link} from "react-router-dom"
-import Cart from './Cart'
-import Home from  './pages/Home'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Cart from "./Cart";
+import Home from "./pages/Home";
 //import About from  './pages/About'
 
-import Products from  './pages/Products'
-import Navigation from './components/Navigation'
+import Products from "./pages/Products";
+import Navigation from "./components/Navigation";
 const App = () => {
   return (
     <>
-     <Router>
-     <Navigation/>
-      
-<Routes>
-       
-
-<Route path="/" component={Home} exact></Route>
-<Home/>
-{/** 
-<Route path="/about" component={About} exact></Route>
-
-<About/>
-*/}
-<Route path="/products" component={Products} exact></Route>
-<Route path="/cart" component={Cart} exact></Route>
-       
-       
-       </Routes>
-
-     </Router>
-
+      <Router>
+        <Navigation />
+        <Home/>
+        <Routes>
+          
+          
+          <Route path="/" component={Home} exact></Route>
+         
+          {/* <Route path="/about" component={About}></Route> */}
+          {/**                            <Route path="/products" exact component={ProductsPage}></Route>
+                            <Route path="/products/:_id" component={SingleProduct}></Route>
+                             */}
+          <Route path="/cart" component={Cart}></Route>
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Product = () => {
+const Product = (props) => {
+  console.log(props);
   return (
     <div>
        <div className="text-center">
